4c8f25a1c0196525fe239e14dc6c9993b82233ed
yuvinrho <yvrho@naver.com>
2023-01-28 02:57:24 +0900

    refactor: CRUDable 프로토콜 리팩토링

--- ProjectManager/ProjectManager/Protocols/CRUDable.swift
+++ ProjectManager/ProjectManager/Protocols/CRUDable.swift
@@ -6,10 +6,10 @@
 //
 
 protocol CRUDable {
-    associatedtype T
+    associatedtype DataType
 
-    func create(data: T)
-    func read() -> [T]
-    func update(data: T)
-    func delete(data: T)
+    func create(data: DataType, completion: @escaping (Result<Bool, Error>) -> ())
+    func read(completion: @escaping (Result<[DataType], Error>) -> ())
+    func update(data: DataType)
+    func delete(data: DataType)
 }
